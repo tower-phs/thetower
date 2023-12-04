0b028c82c67654fda095b075b97e180dcb7b90f4
Jieruei Chang <knosmos@gmail.com>
2023-12-04 13:14:59 -0500

    update archive list

--- pages/archives/index.tsx
+++ pages/archives/index.tsx
@@ -24,20 +24,41 @@ export default function Archives() {
 			`}</style>
 			<h1>Archives</h1>
 			<div className="container">
+				<VirtualArchive month={2} year={2023} />
+				<VirtualArchive month={3} year={2023} />
+				<VirtualArchive month={4} year={2023} />
+				<VirtualArchive month={6} year={2023} />
+				<VirtualArchive month={9} year={2023} />
+				<VirtualArchive month={10} year={2023} />
+				<VirtualArchive month={11} year={2023} />
+				<VirtualArchive month={12} year={2023} />
+
+				<VirtualArchive month={2} year={2022} />
+				<VirtualArchive month={3} year={2022} />
+				<VirtualArchive month={4} year={2022} />
+				<VirtualArchive month={6} year={2022} />
+				<VirtualArchive month={9} year={2022} />
+				<VirtualArchive month={10} year={2022} />
+				<VirtualArchive month={11} year={2022} />
+				<VirtualArchive month={12} year={2022} />
+
 				<VirtualArchive month={2} year={2021} />
 				<VirtualArchive month={3} year={2021} />
+				<VirtualArchive month={4} year={2021} />
+				<VirtualArchive month={6} year={2021} />
 				<VirtualArchive month={9} year={2021} />
 				<VirtualArchive month={10} year={2021} />
 				<VirtualArchive month={11} year={2021} />
 				<VirtualArchive month={12} year={2021} />
-				<VirtualArchive month={2} year={2022} />
-				<VirtualArchive month={3} year={2022} />
-				<VirtualArchive month={4} year={2022} />
-				<VirtualArchive month={9} year={2022} />
-				<VirtualArchive month={10} year={2022} />
-				<VirtualArchive month={11} year={2022} />
-				<VirtualArchive month={2} year={2023} />
-				<VirtualArchive month={3} year={2023} />
+
+				<VirtualArchive month={2} year={2020} />
+				<VirtualArchive month={3} year={2020} />
+				<VirtualArchive month={4} year={2020} />
+				<VirtualArchive month={6} year={2020} />
+				<VirtualArchive month={9} year={2020} />
+				<VirtualArchive month={10} year={2020} />
+				<VirtualArchive month={11} year={2020} />
+				<VirtualArchive month={12} year={2020} />
 			</div>
 		</div>
 	);
