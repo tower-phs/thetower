c1292ae591fb134b231f2e3e8e6f0aaac62e6715
ayush-shrivastava003 <77131808+ayush-shrivastava003@users.noreply.github.com>
2024-03-13 17:09:25 -0400

    fix bug

--- pages/api/id.tsx
+++ pages/api/id.tsx
@@ -7,6 +7,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 	if (req.method != "POST") return res.status(400);
 
 	const category = req.body.cateogry;
-	const id = await getIdOfNewest(category);
+	const id = await getIdOfNewest(category, null);
 	return res.status(200).json(id);
 }
