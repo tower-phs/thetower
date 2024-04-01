45b9fec7ef367c2515c8cbacec6bc95a76234b67
Henry Langmack <Henry.langmack@gmail.com>
2024-04-01 19:17:50 -0400

    games button navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -409,6 +409,8 @@ function NavBar() {
 					<Link href="/category/sports/student-athletes">Student Athletes</Link>
 				</Button>
 
+				<Button name="GAMES" href="/games/crossword"></Button>
+
 				<Button name="ABOUT" href="/about">
 					<Link href="/about/2024">2024 Staff</Link>
 					<hr />
